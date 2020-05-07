c50afcd9a595aadae9a3f608b352a8ef5b4e77fd
unknown <mayami0110@gmail.com>
2020-05-08 02:50:44 +0530

    new java programs

---
+++ src/test/java/BasicJavaPrograms/NumberOfDigit.java
@@ -0,0 +1,21 @@
+package BasicJavaPrograms;
+
+public class NumberOfDigit {
+
+	public static void main(String[] args) {
+		// Print Number of Digit in integer
+		
+		int num = 10;
+		
+		int count =0;
+		
+		while(num>0)
+		{
+			num = num/10;
+			++count;
+		}
+
+		System.out.println(count);
+	}
+
+}
---
+++ src/test/java/BasicJavaPrograms/PrintChars.java
@@ -0,0 +1,17 @@
+package BasicJavaPrograms;
+
+public class PrintChars {
+
+	public static void main(String[] args) {
+		// Print all Char from A to Z
+		
+		char c = 'A';
+		
+		for(char i = c; i<= 'Z';i++ )
+		{
+			System.out.print(i);
+		}
+
+	}
+
+}
